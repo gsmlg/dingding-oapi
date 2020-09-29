595174921aab448db4eca235ac9af9fb75794874
gsmlg <me@gsmlg.org>
2020-09-29 15:59:16 +0800

    gofmt

--- main.go
+++ main.go
@@ -1,51 +1,51 @@
 package main
 
 import (
-    "fmt"
-    "crypto/hmac"
-    "crypto/sha256"
-    "encoding/base64"
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
 	"io/ioutil"
 	"net/http"
-    "net/url"
-	"bytes"
-    "time"
-    "os"
+	"net/url"
+	"os"
+	"time"
 )
 
 func main() {
-    access_token := os.Getenv("access_token")
-    secret := os.Getenv("secret")
+	access_token := os.Getenv("access_token")
+	secret := os.Getenv("secret")
 
-    t := makeTimestamp()
+	t := makeTimestamp()
 
-    signString := fmt.Sprintf("%d\n%s", t, secret)
+	signString := fmt.Sprintf("%d\n%s", t, secret)
 
-    h := hmac.New(sha256.New, []byte(secret))
-    h.Write([]byte(signString))
-    hmac := h.Sum(nil)
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(signString))
+	hmac := h.Sum(nil)
 
-    encoded := base64.StdEncoding.EncodeToString(hmac[:])
+	encoded := base64.StdEncoding.EncodeToString(hmac[:])
 
-    params := url.Values{}
-    params.Add("access_token", access_token)
-    params.Add("timestamp", fmt.Sprintf("%d", t))
-    params.Add("sign", encoded)
+	params := url.Values{}
+	params.Add("access_token", access_token)
+	params.Add("timestamp", fmt.Sprintf("%d", t))
+	params.Add("sign", encoded)
 
-    baseurl := fmt.Sprintf("https://oapi.dingtalk.com/robot/send?%s", params.Encode())
+	baseurl := fmt.Sprintf("https://oapi.dingtalk.com/robot/send?%s", params.Encode())
 
-    msg := os.Args[1:]
+	msg := os.Args[1:]
 
 	r := bytes.NewReader([]byte(fmt.Sprintf("{\"msgtype\": \"text\",\"text\": {\"content\": \"%s\"}}", msg)))
 	resp, err := http.Post(baseurl, "application/json", r)
 	if err != nil {
-        os.Exit(1)
+		os.Exit(1)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-    fmt.Printf("Response: %s", body)
+	fmt.Printf("Response: %s", body)
 }
 
 func makeTimestamp() int64 {
-    return time.Now().UnixNano() / 1e6
+	return time.Now().UnixNano() / 1e6
 }
